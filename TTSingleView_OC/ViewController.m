28c934690216c348b6eb391be23dc3a99beaeab5
shen8686184 <guoliyuan23@sina.com>
2016-12-01 14:41:58 +0800

    ### 2

--- TTSingleView_OC/ViewController.m
+++ TTSingleView_OC/ViewController.m
@@ -37,6 +37,7 @@
     CGRect frame =  CGRectInset(self.view.bounds, 44, 44);
     
     // 1
+    // 2
     
     UIBarButtonItem *rightItem = [UIBarButtonItem lf_barButtonItemWithTitle:@"完成"
                                                                        font:[UIFont systemFontOfSize:16.f]
