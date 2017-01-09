e662393ea658bd74742b12db8de4fd89df9409b4
guoliyuan <guoliyuan@alibaba-inc.com>
2017-01-09 17:22:49 +0800

    ### 10

--- TTSingleView_OC/AppDelegate.m
+++ TTSingleView_OC/AppDelegate.m
@@ -50,7 +50,8 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // 6666666
+
+    // 1000000
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
