86651718e2c9bb7bdb17ab3352e3323041c1f9dd
guoliyuan <guoliyuan@alibaba-inc.com>
2017-01-09 17:20:39 +0800

    ### 7

--- TTSingleView_OC/AppDelegate.m
+++ TTSingleView_OC/AppDelegate.m
@@ -18,7 +18,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    // 1111111
 //    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 //    
 //    UIViewController *vc = [ViewController new];
@@ -33,7 +32,7 @@
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // 444444
+    // 77777
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
